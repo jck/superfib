8e27a0786fdedf7e13dbf2d6e66929da44875c8c
Keerthan Jaic <jckeerthan@gmail.com>
2017-03-01 06:49:14 -0500

    clean up miller-rabin test

--- superfib/primality.py
+++ superfib/primality.py
@@ -4,8 +4,10 @@ def _try_composite(a, d, n, s):
     for i in range(s):
         if pow(a, 2**i * d, n) == n-1:
             return False
-    return True # n  is definitely composite
- 
+    # n is definitely composite
+    return True
+
+
 def is_prime(n, _precision_for_huge_n=16):
     """Miller-Rabin primality test
     https://rosettacode.org/wiki/Miller%E2%80%93Rabin_primality_test#Python
@@ -14,27 +16,30 @@ def is_prime(n, _precision_for_huge_n=16):
         return True
     if any((n % p) == 0 for p in _known_primes):
         return False
+
     d, s = n - 1, 0
     while not d % 2:
         d, s = d >> 1, s + 1
     # Returns exact according to http://primes.utm.edu/prove/prove2_3.html
-    if n < 1373653: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3))
-    if n < 25326001: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5))
-    if n < 118670087467: 
-        if n == 3215031751: 
-            return False
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7))
-    if n < 2152302898747: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11))
-    if n < 3474749660383: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13))
-    if n < 341550071728321: 
-        return not any(_try_composite(a, d, n, s) for a in (2, 3, 5, 7, 11, 13, 17))
-    # otherwise
-    return not any(_try_composite(a, d, n, s) 
+    ranges = [
+        (1373653, (2, 3)),
+        (25326001, (2, 3, 5)),
+        (118670087467, (2, 3, 5, 11)),
+        (2152302898747, (2, 3, 5, 7, 11)),
+        (3474749660383, (2, 3, 5, 7, 11, 13)),
+        (341550071728321, (2, 3, 5, 7, 11, 13, 17)),
+    ]
+    # special case
+    if n == 3215031751:
+        return False
+
+    for upper, bases in ranges:
+        if n < upper:
+            return not any(_try_composite(a, d, n, s) for a in bases)
+
+    return not any(_try_composite(a, d, n, s)
                    for a in _known_primes[:_precision_for_huge_n])
- 
+
+
 _known_primes = [2, 3]
 _known_primes += [x for x in range(5, 1000, 2) if is_prime(x)]
