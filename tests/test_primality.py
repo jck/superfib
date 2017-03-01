53c3b9551dbd9565d9356cd0f483beca4f5657d0
Keerthan Jaic <jckeerthan@gmail.com>
2017-03-01 07:55:50 -0500

    test_primality: add docstrings

--- tests/test_primality.py
+++ tests/test_primality.py
@@ -6,10 +6,14 @@ from superfib.primality import is_prime, known_fib_primes
 
 @pytest.mark.parametrize('n', known_fib_primes)
 def test_known_fib_primes(n):
+    """Check that is_prime works correctly with the precomuted list of fibonacci
+    primes."""
     assert is_prime(n)
 
 
 def test_no_false_negs_in_500():
+    """Ensure that is_prime doesn't have any false negatives within the first
+    500 fibonacci numbers."""
     for n in fibonacci(500):
         if is_prime(n):
             assert n in known_fib_primes
